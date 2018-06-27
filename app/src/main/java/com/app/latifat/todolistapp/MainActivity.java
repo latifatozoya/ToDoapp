3147bc91c96ea93d411e22c92a2f0e34bc82a1bf
Latifat Ozoya <latifat@fb.com>
2018-06-26 18:25:31 -0700

    I was able to list and bring up an edit screen for the todo items and then have any changes to the text reflected in the todo list.

--- app/src/main/java/com/app/latifat/todolistapp/MainActivity.java
+++ app/src/main/java/com/app/latifat/todolistapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.app.latifat.todolistapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -18,6 +19,13 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    //a numeric code to identify edit activity
+    public final static int EDIT_REQUEST_CODE = 20;
+    //keys used to pass data between activities
+    public final static String ITEM_TEXT = "itemText";
+    public final static String ITEM_POSITION = "itemPosition";
+
     ArrayList<String> items;
     ArrayAdapter<String> itemsAdapter;
     ListView lvItems;
@@ -61,7 +69,44 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        //set up item listener for edit (regular click)
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               // create new activity
+                Intent i = new Intent(MainActivity.this, EditItemActivity.class);
+               //pass the data being edited
+                i.putExtra(ITEM_TEXT, items.get(position));
+                i.putExtra(ITEM_POSITION, position);
+               //display the activity
+                startActivityForResult(i, EDIT_REQUEST_CODE);
+            }
+        });
     }
+
+    //handle results from edit activity
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //if the edit completed ok
+        if (resultCode == RESULT_OK && requestCode == EDIT_REQUEST_CODE) {
+            //extract updated item text from result intent extras
+            String updatedItem = data.getExtras().getString(ITEM_TEXT);
+            //extract original position of edited item
+            int position = data.getExtras().getInt(ITEM_POSITION);
+            //update the model with the new item text at the edited position
+            items.set(position, updatedItem);
+            //notify the adapter that the model changed
+            itemsAdapter.notifyDataSetChanged();
+            //persist the changed model
+            writeItems();
+            //notify the user the operation is completed ok
+            Toast.makeText(this, "Items updated successfully", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private File getDataFile() {
         return new File(getFilesDir(), "todo.txt");
     }
